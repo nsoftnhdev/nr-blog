62bcb92db4dc5dc2882947ee3059efd58052a893
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-01-11 09:18:26 +0800

    updated

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,6 +1,5 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
 
 export async function POST() {
   const SIGNING_SECRET = process.env.SIGNING_SECRET
