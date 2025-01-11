b327ebec6e47245316a1f37a9da362435582cf11
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-01-11 19:10:10 +0800

    creating or updating user

---
+++ src/lib/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import { use } from "react";
+
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profilePicture: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model.User || mongoose.model("User", userSchema);
+
+export default User;
