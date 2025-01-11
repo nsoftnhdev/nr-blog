7818f2271d3264bcba80e89c05f8b8a4d12eddf6
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-01-11 21:05:08 +0800

    fix error

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model";
 
-import { connect } from "../mongodb/mongoose.js";
+import { connect } from "../mongodb/mongoose";
 
 export const createOrUpdateUser = async (
   id,
