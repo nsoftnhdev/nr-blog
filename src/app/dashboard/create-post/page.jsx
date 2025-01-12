3cf29b91a994ba1e68fe7886869591a291cf792a
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-01-12 14:39:23 +0800

    Add the create post page UI and protect it as an admin

--- src/app/dashboard/create-post/page.jsx
+++ src/app/dashboard/create-post/page.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { Button, FileInput, Select, TextInput } from "flowbite-react";
+
+import dynamic from "next/dynamic";
+
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+import 'react-quill-new/dist/quill.snow.css';
 
 const CreatePostPage = () => {
-  return (
-    <div>CreatePostPage</div>
-  )
-}
+  const { isSignedIn, user, isLoaded } = useUser();
 
-export default CreatePostPage
+  if (!isLoaded) {
+    return null;
+  }
+
+  if (isSignedIn && user.publicMetadata.isAdmin) {
+    return (
+      <div className="p-3 max-w-3xl mx-auto min-h-screen">
+        <h1 className="text-center text-3xl my-7 font-semibold">
+          Create Post Page
+        </h1>
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 sm:flex-row justify-between">
+            <TextInput
+              type="text"
+              placeholder="Title"
+              required
+              id="title"
+              className="flex-1"
+            />
+            <Select>
+              <option value="uncategorized">Select a category</option>
+              <option value="javascript">JavaScript</option>
+              <option value="reactjs">React.js</option>
+              <option value="nextjs">Next.js</option>
+            </Select>
+          </div>
+          <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
+            <FileInput type="file" accept="image/*" />
+            <Button
+              type="button"
+              gradientDuoTone="tealToLime"
+              size="sm"
+              outline
+            >Upload Image</Button>
+          </div>
+          <ReactQuill
+            theme="snow"
+            placeholder="Write something..."
+            className="h-72 mb-12"
+            required
+          />
+          <Button type="submit" gradientDuoTone="tealToLime">
+            Publish
+          </Button>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <h1 className="text-center text-3xl my-7 font-semibold">
+        You are not authorized to view this page
+      </h1>
+    );
+  }
+};
+
+export default CreatePostPage;
